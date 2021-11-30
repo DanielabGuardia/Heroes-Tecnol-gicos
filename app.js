73fb24886a1a90ec5113df8d26d7b912a847fb1d
DanielabGuardia <danielabguardia@gmail.com>
2021-11-30 14:01:42 -0300

    prueba

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+// @ts-nocheck
 
 let express = require('express');
 let app = express();
 let path = require('path');
 const PORT = 3030;
 
+// @ts-ignore
 app.use(express.static("public"))
 
+// @ts-ignore
 app.get('/', function (req, res) {
+    // @ts-ignore
     res.sendFile(path.join(__dirname, 'views/index.html' ))
 })
 
