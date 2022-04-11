0463471fb5c6eb2288dcfc26d54db557dc0bd6df
DanielabGuardia <danielabguardia@gmail.com>
2022-04-11 13:28:31 -0300

    package

--- app.js
+++ app.js
@@ -3,22 +3,55 @@
 let express = require('express');
 let app = express();
 let path = require('path');
-const PORT = 3030;
+const PORT = 3001;
 
-// @ts-ignore
+
 app.use(express.static("public"))
 
-// @ts-ignore
 app.get('/', function (req, res) {
-    // @ts-ignore
-    res.sendFile(path.join(__dirname, 'views/index.html' ))
+    res.sendFile(path.join(__dirname, '/views/index.html'))
 })
+
+app.get('/babbage', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views/babbage.html'));
+})
+
+app.get('/berners-lee', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views/berners-lee.html'));
+})
+
+app.get('/clarcke', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views/clarcke.html'));
+})
+
+app.get('/hamilton', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views/hamilton.html'));
+})
+
+app.get('/hopper', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views/hopper.html'))
+})
+
+app.get('/lovelace', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views/lovelace.html'))
+})
+
+app.get('/turing', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views/turing.html'))
+})
+
+
+
+
+
+
+
+
+
+
 
 
 app.listen(PORT, () => console.log(`
 Servidor levantado en el puerto ${PORT}
 http://localhost:${PORT}
 `))
-
-/* */
-
